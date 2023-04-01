4ab3e9004f66c63a8b1932c1866bc97abb352ea4
nesrines <nasrinfa@code.edu.az>
2023-04-01 17:46:14 +0400

    added Task-2.cs

---
+++ Task-2.cs
@@ -0,0 +1,15 @@
+﻿int[] Numbers = { 73, 58, -96, 0, 28, -67 };
+int PositiveCount = 0;
+int NegativeCount = 0;
+
+for (int i = 0; i < Numbers.Length; i++)
+{
+    if (Numbers[i] > 0)
+    { PositiveCount++; }
+
+    else if (Numbers[i] < 0)
+    { NegativeCount++; }
+}
+
+Console.WriteLine(PositiveCount + " positive numbers");
+Console.WriteLine(NegativeCount + " negative numbers");

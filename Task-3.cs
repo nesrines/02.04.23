136a58992754229f2b81dee1225c8578a8c917b6
nesrines <nasrinfa@code.edu.az>
2023-04-01 17:59:56 +0400

    added Task-3.cs

---
+++ Task-3.cs
@@ -0,0 +1,11 @@
+﻿int[] Numbers = {1, 3, 2, 6, 7, 4, 45, 36};
+int max = 0;
+
+for (int i = 0; i < Numbers.Length; i++)
+{
+    Console.WriteLine(Numbers[i]);
+    if (Numbers[i] > max)
+    { max = Numbers[i]; }
+}
+
+Console.WriteLine("Greatest number = " + max);

501911d37eab6ffb03a02c6755f0747aa385075d
nesrines <nasrinfa@code.edu.az>
2023-04-01 17:16:21 +0400

    added Task-1.cs

---
+++ Task-1.cs
@@ -0,0 +1,36 @@
+﻿int score = 1;
+
+if (score < 0 || score > 100)
+{
+    Console.WriteLine("invalid");
+}
+
+else if (score > 90)
+{
+    Console.WriteLine("A");
+}
+
+else if (score > 80)
+{
+    Console.WriteLine("B");
+}
+
+else if (score > 70)
+{
+    Console.WriteLine("C");
+}
+
+else if (score > 60)
+{
+    Console.WriteLine("D");
+}
+
+else if (score > 50)
+{
+    Console.WriteLine("E");
+}
+
+else
+{
+    Console.WriteLine("Failed");
+}
